4699ab1d488a79b2256247b35003907fc50cca85
maarcus_andersson <marcus.andersson@stud.sti.se>
2025-10-24 15:26:39 +0200

    add Shape class for Lab2

---
+++ lab2/shape.py
@@ -0,0 +1,32 @@
+class Shape:
+
+    def __init__(self, x: float, y: float):
+
+        self.x = x
+        self.y = y
+
+    def translate(self, dx, dy):
+        """Translates (moves) the shape by a given offset in the x and y directions.
+        This method updates the current position of the shape by adding dx and dy
+        to its existing coordinates."""
+
+        self.x += dx
+        self.y += dy
+
+    @property
+    def area(self) -> None:
+
+        pass
+
+    @property
+    def perimeter(self) -> None:
+
+        pass
+
+    def __repr__(self):
+
+        return f"Shape x={self.x}, y={self.y}"
+
+    def __str__(self):
+
+        return f"Shape is positioned at coordinates (x={self.x}, y={self.y})"
