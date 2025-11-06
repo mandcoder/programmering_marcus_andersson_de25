2394e6ea56922f8bdfbe1aa8aeb9ae018ccf7e87
maarcus_andersson <marcus.andersson@stud.sti.se>
2025-11-06 13:05:28 +0100

    properties added for x and y

--- lab2/shape.py
+++ lab2/shape.py
@@ -1,7 +1,9 @@
 class Shape:
+    """Base class for all 2D shapes."""
 
     def __init__(self, x: float, y: float):
 
+        # initialize coordinates for the shape
         self.x = x
         self.y = y
 
@@ -15,43 +17,81 @@ class Shape:
 
     @property
     def area(self) -> None:
-
+        # abstract property (to be implemented in subclasses)
         pass
 
     @property
     def perimeter(self) -> None:
-
+        # abstract property (to be implemented in subclasses)
         pass
 
     def __repr__(self):
-
+        # developer-friendly string representation ( for debugging)
         return f"Shape x={self.x}, y={self.y}"
 
     def __str__(self):
-
+        # user-friendly description of the shapes position
         return f"Shape is positioned at coordinates (x={self.x}, y={self.y})"
 
-    # overload < operator
+    # function to check if other is a shape
+    def check_shape(self, other):
+
+        if not isinstance(other, Shape):
+
+            return NotImplemented
+        else:
+            return True
+
+    # six overload operators with comparision based on area
     def __lt__(self, other):
 
-        return self.area < other.area
+        # less than (<)
+        if self.check_shape(other):
+
+            return self.area < other.area
+        else:
+            return NotImplemented
 
     def __le__(self, other):
 
-        return self.area <= other.area
+        if self.check_shape(other):
+
+            return self.area <= other.area
+
+        else:
+            return NotImplemented
 
     def __eq__(self, other):
 
-        return self.area == other.area
+        if self.check_shape(other):
+
+            return self.area == other.area
+
+        else:
+            return NotImplemented
 
     def __ne__(self, other):
 
-        return self.area != other.area
+        if self.check_shape(other):
+
+            return self.area != other.area
+
+        else:
+            return NotImplemented
 
     def __gt__(self, other):
 
-        return self.area > other.area
+        if self.check_shape(other):
+
+            return self.area > other.area
+
+        else:
+            return NotImplemented
 
     def __ge__(self, other):
 
-        return self.area >= other.area
+        if self.check_shape(other):
+
+            return self.area >= other.area
+        else:
+            return NotImplemented
