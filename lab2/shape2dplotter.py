6b9ca04508f6ec957c097d7d3d7176b0bfc69dfb
maarcus_andersson <marcus.andersson@stud.sti.se>
2025-11-07 21:12:23 +0100

    some comments are added

--- lab2/shape2dplotter.py
+++ lab2/shape2dplotter.py
@@ -13,7 +13,7 @@ class Shape2dPlotter:
     with consistent axis limits and grid.
     """
 
-    def __init__(self, shapes=None):
+    def __init__(self, shapes: list[Shape] | None = None):
 
         if shapes is None:
             self.shapes = []
@@ -36,25 +36,23 @@ class Shape2dPlotter:
     def draw_all(self):
 
         for shape in self.shapes:
-
             if isinstance(shape, Circle):
-
                 circle_patch = matp_circle((shape.x, shape.y), shape.radius, fill=False)
-
                 self.ax.add_patch(circle_patch)
-
             if isinstance(shape, Rectangle):
-
                 rectangle_patch = matp_rectangle(
                     (shape.x, shape.y), shape.width, shape.height, fill=False
                 )
-
                 self.ax.add_patch(rectangle_patch)
 
-        # setting limit on x and y
+        # Set axis limits and proportions for the plot
         self.ax.set_xlim(-5, 20)
         self.ax.set_ylim(-5, 20)
+
+        # keeps circles and rectangles correctly scaled
         self.ax.set_aspect("equal")
+
+        # Add a light dashed grid for better readability
         self.ax.grid(True)
         self.ax.grid(color="gray", linestyle="--", linewidth=0.5)
         plt.show()
